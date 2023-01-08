5f775997a6011070eb42270be8ec6eb6ab04ebc2
Erick <centeno222@live.com>
2023-01-08 17:01:18 +0100

    Create second.js

---
+++ javaScript/ejercicio_2/second.js
@@ -0,0 +1,15 @@
+let name = "Erick";
+let age = 19;
+let amIDevelper = true;
+let birthday = new Date("february 22 2003");
+const book = {
+    name:"Guia del autoestopista galactico",
+    author:"Douglas Adams",
+    date:"octubre  1979",
+    url:"https://www.casadellibro.com/libro-guia-del-autoestopista-galactico-6-ed/9788433973108/1173636"
+}
+
+
+
+let lista = [name, age, amIDevelper, birthday, book ];
+console.log(lista);
