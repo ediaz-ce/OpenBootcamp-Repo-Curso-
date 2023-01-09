4df32c7cb3dcac3db6ec9e3150db6d90f9782034
Erick <centeno222@live.com>
2023-01-09 12:05:06 +0100

    ejercicio_5

---
+++ javaScript/ejercicio_5/numeros.js
@@ -0,0 +1,10 @@
+let alturaCentimetros = 174;
+let alturaMetros = 1.74;
+let pesoKilogramos = 72.05
+let alturaMetrosRedArriba = Math.ceil(alturaMetros);
+let pesoKilogramosRedAbajo = Math.floor(pesoKilogramos)
+let maxNum = Number.MAX_VALUE;
+let maxNumMasUno = Number.MAX_VALUE + 1;
+
+let esIgual = (maxNum == maxNumMasUno)
+
