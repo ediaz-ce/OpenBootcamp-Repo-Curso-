c953e2b46b886773e10ce148f1f0b5435ab10bb6
Erick <centeno222@live.com>
2023-01-10 19:49:29 +0100

    ejercicio_12

---
+++ javaScript/ejercicio_12/index.js
@@ -0,0 +1,33 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    // defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+
+function errorPer (valor){
+    if(typeof valor === "string"){
+        return "Valor admitido";
+    }
+    throw new Error("Valor no admitido");
+}
+let prueba = "hola";
+
+try{
+    const val = errorPer(prueba);
+    logger.log(val);
+}
+catch(e){
+    logger.error(e);
+}
